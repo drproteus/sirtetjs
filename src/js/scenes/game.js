3b2c415b630d8b8ca47b3cef38ec77bfa5c3abd1
Jake Goritski <jakeg@proton.me>
2026-06-18 18:54:59 -0700

    working on collision events, todo writing piece

--- src/js/scenes/game.js
+++ src/js/scenes/game.js
@@ -1,6 +1,14 @@
 import Phaser from "phaser"
 import { Piece, ZPiece, SPiece, IPiece, LPiece, JPiece, OPiece } from "../lib/pieces"
 
+const EVENT = {
+  NO_COLLISIONS: 0,
+  LEFT_WALL_COLLIDE: 1,
+  RIGHT_WALL_COLLIDE: 2,
+  FLOOR_COLLIDE: 3,
+  TILE_COLLIDE: 4,
+}
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" })
@@ -50,19 +58,64 @@ class GameScene extends Phaser.Scene {
     this.input.keyboard.on("keydown-X", this.rotateCW, this)
     this.input.keyboard.on("keydown-C", this.rotateCCW, this)
     this.cursors = this.input.keyboard.createCursorKeys()
+    this.movingX = false;
   }
   update(t, delta) {
+    let newX = this.piece.x
+    let newY = this.piece.y
     this.fallTime += delta / 1000.0
     if (this.fallTime >= this.fallSpeed) {
-      this.piece.y += 1
+      newY += 1
       this.fallTime = 0
     }
+    if (!this.movingX) {
+      if (this.cursors.right.isDown) {
+        newX += 1;
+        this.movingX = true;
+      } else if (this.cursors.left.isDown) {
+        newX -= 1;
+        this.movingX = true;
+      }
+    } else if (this.cursors.right.isUp && this.cursors.left.isUp) {
+      this.movingX = false;
+    }
+    if (this.checkCollisions(newX, newY)) {
+      console.log("COLLISIONS!")
+      this.movingX = false
+      return
+    }
+    this.piece.x = newX
+    this.piece.y = newY
     this.playfieldMap.destroyLayer(this.pieceLayer)
     this.pieceLayer = this.playfieldMap.createBlankLayer(1, this.playfieldTiles, 16, 0)
     this.pieceLayer.putTilesAt(this.piece.render(), this.piece.x, this.piece.y)
   }
-  checkCollisions(newX, newY) {
-    return false
+  checkCollisions(x, y) {
+    let s = this.piece.getShape()
+    for (let i = 0; i < s[0].length; i++) {
+      for (let j = 0; j < s.length; j++) {
+        if (s[j][i] < 1) continue
+        if (x + i < 0) {
+          // collides with left side of playfield
+          console.log("Collide with LEFT")
+          return EVENT.LEFT_WALL_COLLIDE
+        } else if (x + i >= this.widthTiles) {
+          // collides with right side of playfield
+          console.log("Collide with RIGHT")
+          return EVENT.RIGHT_WALL_COLLIDE
+        } else if (y + j + 1 > this.heightTiles) {
+          // hit the floor of the playfield
+          console.log("Collid with FLOOR")
+          return EVENT.FLOOR_COLLIDE
+        } else if (this.playfield[y + j][x + i] != -1) {
+          // collides with filled tile
+          // TODO treat this special?
+          console.log("Collide with TILE", x + i, y + j, this.playfield[x + i][y + j])
+          return EVENT.TILE_COLLIDE
+        }
+      }
+    }
+    return EVENT.NO_COLLISIONS
   }
   rotateCW() {
     console.log("Rotating CW")
